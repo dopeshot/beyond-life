36c1c5da86f1d0f742afbaf07eacb779feb5e7f2
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-04-05 13:32:39 +0200

    Ref: remove semicolons in formatting rules + lint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { semi: false, endOfLine: 'auto' }],
   },
 };
