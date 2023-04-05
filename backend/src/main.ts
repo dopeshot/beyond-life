9b7d0f6cfe4045aa0c9015220ad133828a18ce7e
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-04-05 14:06:16 +0200

    Add: Swagger+validationpipes to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,34 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  )
 
-  await app.listen(3001)
+  // Swagger
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('BeyondLife Api')
+      .setDescription('The BeyondLife Api')
+      .setVersion('0.1')
+      .build()
+    const document = SwaggerModule.createDocument(app, config, {
+      ignoreGlobalPrefix: false,
+    })
+    SwaggerModule.setup('swagger', app, document)
+  }
+
+  app.enableCors()
+
+  await app.listen(process.env.PORT || 3001)
 }
 
 bootstrap()
