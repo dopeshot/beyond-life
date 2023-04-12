f6136cb2ab48faf5425fbcfd3b9b8c57ec7f4277
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-04-12 19:08:58 +0200

    feat(db): cleanup

--- backend/src/db/db.service.ts
+++ backend/src/db/db.service.ts
@@ -1,23 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client'
-
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
+import { PrismaClient, User } from '@prisma/client'
 
 @Injectable()
 export class DbService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
   }
 
   // Never called, only used for validating the setup works
-  private typeValidation(){
-	 return this.prisma.user.findUnique({
-      where: userWhereUniqueInput,
-    });  }
+  private async typeValidation(): Promise<void> {
+    const user = await this.user.findFirst({})
+    console.log(user.email)
+  }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
-      await app.close();
-    });
+      await app.close()
+    })
   }
-} 
-
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DbService } from './db/db.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   // Ensure save shutdown
-  const prismaService = app.get(PrismaService);
+  const prismaService = app.get(DbService)
   await prismaService.enableShutdownHooks(app)
 
   await app.listen(3001)
