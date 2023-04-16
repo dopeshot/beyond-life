d6b6481577438257155a18eaa9217711bccd2bf0
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-04-16 12:21:43 +0200

    feat(db): add typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get<string>('DB_USER_NAME'),
+        password: configService.get<string>('DB_USER_PW'),
+        database: configService.get<string>('DB_DB'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,14 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { DataSource } from 'typeorm'
+import { setupDataSource } from './helpers/db.helper'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
   beforeEach(async () => {
+    const dataSource = await setupDataSource();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile()
+    })
+    .overrideProvider(DataSource)
+    .useValue(dataSource)
+    .compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
