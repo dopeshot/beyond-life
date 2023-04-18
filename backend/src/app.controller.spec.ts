2c01dfa456c4f7781b77dffecc9056270ca8d327
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-04-18 22:26:21 +0200

    Fix: tests

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+      expect(appController.getHello()).toBe({ first: 'Hello', second: 'World' })
     })
   })
 })
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,21 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
 import { DataSource } from 'typeorm'
+import { AppModule } from './../src/app.module'
 import { setupDataSource } from './helpers/db.helper'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
   beforeEach(async () => {
-    const dataSource = await setupDataSource();
+    const dataSource = await setupDataSource()
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
-    .overrideProvider(DataSource)
-    .useValue(dataSource)
-    .compile()
+      .overrideProvider(DataSource)
+      .useValue(dataSource)
+      .compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
@@ -25,6 +25,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!')
+      .expect({ first: 'Hello', second: 'World' })
   })
 })
