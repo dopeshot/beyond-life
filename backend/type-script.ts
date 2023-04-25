af3c6a5fd142f748bc4cefd89287357c3536ec97
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-04-25 15:19:57 +0200

    devops(typegen): add type generation pipeline

---
+++ backend/type-script.ts
@@ -0,0 +1,33 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './src/app.module';
+import * as yaml from 'yaml';
+import * as fs from 'fs';
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { setupDataSource } from './test/helpers/db.helper';
+import { exec } from 'child_process';
+
+async function main() {
+  const dataSource = await setupDataSource();
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  })
+    .overrideProvider(DataSource)
+    .useValue(dataSource)
+    .compile();
+
+  const app = moduleFixture.createNestApplication();
+  const config = new DocumentBuilder().build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  const yamlString = yaml.stringify(document, {});
+  fs.writeFileSync('./api.yaml', yamlString);
+
+  exec('npx openapi-typescript schema.yaml --output types.ts');
+  fs.rmSync('./api.yaml');
+}
+
+main();
---
+++ typegen.sh
@@ -0,0 +1,2 @@
+( cd backend && npm run gen:types ) > /dev/null
+cp backend/schema.ts frontend
