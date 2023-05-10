41f39a40e16f680c33e9bd051bf87cb10b0b5968
Marvin Pfau <marvin.pfau@stihl.de>
2023-05-10 20:00:27 +0200

    remove color

--- frontend/src/components/Toggle/Toggle.tsx
+++ frontend/src/components/Toggle/Toggle.tsx
@@ -24,7 +24,7 @@ export const Toggle: React.FC<ToggleProps> = ({ name, labelText, labelOn, labelO
             <label className="block text-darkgrey text-sm font-semibold mb-1" htmlFor={name}>{labelText}{labelRequired && <span className="text-primary-blue ml-1">*</span>}</label>
             <Field name={name}>{(props: FieldProps<any>) => (
                 <div data-cy={`${name}-clickdiv`} onClick={() => props.form.setFieldValue(name, !props.field.value)} className="inline-flex items-center cursor-pointer">
-                    <div className={`w-14 h-7 flex items-center ${props.field.value ? "bg-yellow-500" : "bg-gray-300"} rounded-full p-1 duration-300 ease-in-out my-1 ${props.field.value ? "bg-primary-blue" : ""}`}>
+                    <div className={`w-14 h-7 flex items-center ${props.field.value ? "bg-yellow-500" : "bg-gray-300"} rounded-full p-1 duration-300 ease-in-out my-1`}>
                         <div data-cy={`${name}-ball`} className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${props.field.value ? "translate-x-7" : ""}`}></div>
                     </div>
                     <p data-cy={`${name}-labeltext`} className="text-darkgrey text-sm font-semibold pl-2">{props.field.value ? labelOn : labelOff}</p>
