2630386a09d61c32995ccd720a9ecb170dc43a43
LarsFlieger <a.purenetwork@gmail.com>
2023-05-15 12:40:13 +0200

    chore(types): Fix typ bug in checkbox tests

--- frontend/src/components/Checkbox/Checkbox.cy.tsx
+++ frontend/src/components/Checkbox/Checkbox.cy.tsx
@@ -1,16 +1,17 @@
-import { mount } from "cypress/react"
-import { Form, Formik } from "formik"
-import { Checkbox } from "./Checkbox"
+import { mount } from 'cypress/react'
+import { Form, Formik } from 'formik'
+import { ChildrenProps } from '../../types/children'
+import { Checkbox } from './Checkbox'
 
 const initialValues = {
-	category: "",
+	category: '',
 }
 
 const submitForm = (values: typeof initialValues) => {
 	console.log(values)
 }
 
-const Wrapper: React.FC = ({ children }) => {
+const Wrapper: React.FC<ChildrenProps> = ({ children }) => {
 	return (
 		<Formik initialValues={initialValues} onSubmit={submitForm}>
 			{() => <Form>{children}</Form>}
@@ -18,25 +19,25 @@ const Wrapper: React.FC = ({ children }) => {
 	)
 }
 
-describe("Checkbox", () => {
+describe('Checkbox', () => {
 	const data = {
-		name: "label",
-		labelText: "Labels",
-		helperText: "Die Labels können Gerichten hinzugefügt werden.",
+		name: 'label',
+		labelText: 'Labels',
+		helperText: 'Die Labels können Gerichten hinzugefügt werden.',
 		options: [
 			{
 				id: 1,
-				label: "vegan",
+				label: 'vegan',
 			},
 			{
 				id: 2,
-				label: "vegetarisch",
-				icon: "iconExample",
+				label: 'vegetarisch',
+				icon: 'iconExample',
 			},
 			{
 				id: 3,
-				label: "ohne Schweinefleisch",
-				icon: "iconExample",
+				label: 'ohne Schweinefleisch',
+				icon: 'iconExample',
 			},
 		],
 	}
@@ -55,51 +56,35 @@ describe("Checkbox", () => {
 		)
 	})
 
-	it("should set labeltext", () => {
-		cy.get("h5 ").should("contain", data.labelText)
+	it('should set labeltext', () => {
+		cy.get('h5 ').should('contain', data.labelText)
 	})
 
-	it("should set helper text", () => {
-		cy.get(`[data-cy="${data.name}-helpertext"]`).should(
-			"contain",
-			data.helperText
-		)
+	it('should set helper text', () => {
+		cy.get(`[data-cy="${data.name}-helpertext"]`).should('contain', data.helperText)
 	})
 
-	it("should display all options", () => {
+	it('should display all options', () => {
 		data.options.forEach((option) => {
-			cy.get(`[data-cy="${data.name}-option-${option.id}"]`).should(
-				"contain",
-				option.label
-			)
+			cy.get(`[data-cy="${data.name}-option-${option.id}"]`).should('contain', option.label)
 		})
 	})
 
-	it("should check box when click on it", () => {
-		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`)
-			.check()
-			.should("be.checked")
-		cy.get(
-			`[data-cy="${data.name}-option-${data.options[1].id}"] input`
-		).should("not.be.checked")
+	it('should check box when click on it', () => {
+		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`).check().should('be.checked')
+		cy.get(`[data-cy="${data.name}-option-${data.options[1].id}"] input`).should('not.be.checked')
 	})
 
-	it("should uncheck box when click on it after it is checked", () => {
-		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`)
-			.check()
-			.should("be.checked")
-		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`)
-			.click()
-			.should("not.be.checked")
+	it('should uncheck box when click on it after it is checked', () => {
+		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`).check().should('be.checked')
+		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] input`).click().should('not.be.checked')
 	})
 
-	it("should display icon before label when defined", () => {
+	it('should display icon before label when defined', () => {
 		// TODO: Wenn wir ne Icon Lösung haben
 	})
 
-	it("should not display icon before label when not defined", () => {
-		cy.get(
-			`[data-cy="${data.name}-option-${data.options[0].id}"] svg`
-		).should("not.exist")
+	it('should not display icon before label when not defined', () => {
+		cy.get(`[data-cy="${data.name}-option-${data.options[0].id}"] svg`).should('not.exist')
 	})
 })
--- frontend/src/components/Checkbox/Checkbox.tsx
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { Field } from "formik"
+import { Field } from 'formik'
 
 type CheckboxProps = {
 	/** Provide an name to uniquely identify the Checkbox input. */
