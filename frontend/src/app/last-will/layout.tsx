8b3980150216e8173db1700ba905cafc16b1b3a9
TjWaaaa <timowaldherr98@gmail.com>
2023-05-17 13:17:16 +0200

    Revert "feat: add placeholder component"
    This reverts commit 7940f5126cc21fa9431549b5353aafe797f06288.

--- frontend/src/app/last-will/layout.tsx
+++ frontend/src/app/last-will/layout.tsx
@@ -1,26 +1,19 @@
 import { GlobalFooter } from '../../components/Navbar/GlobalFooter/GlobalFooter'
 import { ModuleNavbar } from '../../components/Navbar/ModuleNavbar/ModuleNavbar'
-import { Placeholder } from '../../components/Placeholder/Placeholder'
 import { fontPlusJakartaSans } from '../../services/font/font'
 import './../globals.css'
 
 export const metadata = {
-	title: 'Beyond Life',
-	description: 'Handle your death.'
+    title: 'Beyond Life',
+    description: 'Handle your death.',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body className={`flex flex-col min-h-screen ${fontPlusJakartaSans.className}`}>
-				<ModuleNavbar />
-				{children}
-				<Placeholder
-					name="content"
-					className="h-10 w-10 "
-				/>
-				<GlobalFooter />
-			</body>
-		</html>
-	)
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+    return <html lang="en">
+        <body className={`flex flex-col min-h-screen ${fontPlusJakartaSans.className}`}>
+            <ModuleNavbar />
+            {children}
+            <GlobalFooter />
+        </body>
+    </html>
 }
