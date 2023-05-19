67cac2416e142d94a7ac3a404acf46b5652f661f
TjWaaaa <timowaldherr98@gmail.com>
2023-05-19 14:53:05 +0200

    feat: use Label component in Toggle

--- frontend/src/components/Toggle/Toggle.tsx
+++ frontend/src/components/Toggle/Toggle.tsx
@@ -1,36 +1,65 @@
-import { Field, FieldProps } from "formik"
+import { Field, FieldProps } from 'formik'
+import { Label } from '../Form/Label/Label'
 
 export type ToggleProps = {
-    /** Gives the toggle a unique name. */
-    name: string
-    /** Provide label text. */
-    labelText: string
-    /** Specify the label for the "on" position. */
-    labelOn?: string
-    /** Specify the label for the "off" position. */
-    labelOff?: string
-    /** When set Required * will be seen. */
-    labelRequired?: boolean
-    /** Provide text that is used alongside the control label for additional help. */
-    helperText?: string
+	/** Gives the toggle a unique name. */
+	name: string
+	/** Provide label text. */
+	labelText: string
+	/** Specify the label for the "on" position. */
+	labelOn?: string
+	/** Specify the label for the "off" position. */
+	labelOff?: string
+	/** When set Required * will be seen. */
+	inputRequired?: boolean
+	/** Provide text that is used alongside the control label for additional help. */
+	helperText?: string
 }
 
 /**
  * Toggle, can only be used with Formik.
  */
-export const Toggle: React.FC<ToggleProps> = ({ name, labelText, labelOn, labelOff, helperText, labelRequired = false }) => {
-    return (
-        <div className="mb-4">
-            <label className="block text-sm mb-1" htmlFor={name}>{labelText}{labelRequired && <span className=" text-yellow-500 ml-1">*</span>}</label>
-            <Field name={name}>{(props: FieldProps<boolean>) => (
-                <div data-cy={`${name}-clickdiv`} onClick={() => props.form.setFieldValue(name, !props.field.value)} className="inline-flex items-center cursor-pointer">
-                    <div className={`w-14 h-7 flex items-center ${props.field.value ? "bg-yellow-500" : "bg-gray-300"} rounded-full p-1 duration-300 ease-in-out my-1`}>
-                        <div data-cy={`${name}-ball`} className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${props.field.value ? "translate-x-7" : ""}`}></div>
-                    </div>
-                    <p data-cy={`${name}-labeltext`} className="text-sm font-semibold pl-2">{props.field.value ? labelOn : labelOff}</p>
-                </div>
-            )}</Field>
-            <p data-cy={`${name}-helpertext`} className="text-sm text-gray-500">{helperText}</p>
-        </div>
-    )
+export const Toggle: React.FC<ToggleProps> = ({
+	name,
+	labelText,
+	labelOn,
+	labelOff,
+	helperText,
+	inputRequired = false
+}) => {
+	return (
+		<div className="mb-4">
+			{labelText && (
+				<Label datacy={`textinput-${name}`} name={name} labelText={labelText} inputRequired={inputRequired} />
+			)}
+			<Field name={name}>
+				{(props: FieldProps<boolean>) => (
+					<div
+						data-cy={`${name}-clickdiv`}
+						onClick={() => props.form.setFieldValue(name, !props.field.value)}
+						className="inline-flex items-center cursor-pointer"
+					>
+						<div
+							className={`w-14 h-7 flex items-center ${
+								props.field.value ? 'bg-yellow-500' : 'bg-gray-300'
+							} rounded-full p-1 duration-300 ease-in-out my-1`}
+						>
+							<div
+								data-cy={`${name}-ball`}
+								className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${
+									props.field.value ? 'translate-x-7' : ''
+								}`}
+							></div>
+						</div>
+						<p data-cy={`${name}-labeltext`} className="text-sm font-semibold pl-2">
+							{props.field.value ? labelOn : labelOff}
+						</p>
+					</div>
+				)}
+			</Field>
+			<p data-cy={`${name}-helpertext`} className="text-sm text-gray-500">
+				{helperText}
+			</p>
+		</div>
+	)
 }
