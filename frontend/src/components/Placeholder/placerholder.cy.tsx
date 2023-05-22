3b177fbcaec83922f3b9e4d15e9e9ddf9a0e5040
LarsFlieger <a.purenetwork@gmail.com>
2023-05-22 12:56:09 +0200

    chore(tests): improved performance of tests by removing twice mounting

--- frontend/src/components/Placeholder/placerholder.cy.tsx
+++ frontend/src/components/Placeholder/placerholder.cy.tsx
@@ -17,12 +17,12 @@ describe('Placeholder', () => {
 		it('should display correct name', () => {
 			cy.datacy(DATACY).contains(PLACEHOLDER_NAME).should('be.visible')
 		})
+	})
 
-		it('should have additional class when className is set', () => {
-			const CLASS_NAME = 'text-red-500'
-			cy.mount(<Placeholder name="Test" className={CLASS_NAME} datacy={DATACY} />)
-			cy.datacy(DATACY).should('have.class', CLASS_NAME)
-		})
+	it('should have additional class when className is set', () => {
+		const CLASS_NAME = 'text-red-500'
+		cy.mount(<Placeholder name="Test" className={CLASS_NAME} datacy={DATACY} />)
+		cy.datacy(DATACY).should('have.class', CLASS_NAME)
 	})
 })
 
