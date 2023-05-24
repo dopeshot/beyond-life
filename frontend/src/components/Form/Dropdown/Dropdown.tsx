1d7517681944444f0c283d53f59e65a3b3013285
Marvin Pfau <marvin.pfau@stihl.de>
2023-05-24 16:24:27 +0200

    change labelRequired Name

--- frontend/src/components/Form/Dropdown/Dropdown.tsx
+++ frontend/src/components/Form/Dropdown/Dropdown.tsx
@@ -13,7 +13,7 @@ type DropdownProps = {
 	/** Inform users what the corresponding input fields mean. */
 	labelText?: string
 	/** When set Required * will be seen.  */
-	labelRequired?: boolean
+	inputRequired?: boolean
 	/** A list of options to choose from. */
 	options: readonly ComponentOptions[]
 	/** Set to true when dropdown needs to fit to other inputs (have lighter color). */
@@ -26,7 +26,7 @@ type DropdownProps = {
  * Dropdown, can only be used with Formik.
  * @example <Dropdown name="Wähle dein Geschlecht" labelText="Geschlecht" options={[{ value: "1", label: "Männlich" }, { value: "2", label: "Weiblich" }]} />
  */
-export const Dropdown: React.FC<DropdownProps> = ({ name, placeholder, labelText, labelRequired, light = false, hasMargin = false, options }) => {
+export const Dropdown: React.FC<DropdownProps> = ({ name, placeholder, labelText, inputRequired, light = false, hasMargin = false, options }) => {
 	const [isOpen, setIsOpen] = useState(false)
 
 	return (
@@ -37,7 +37,7 @@ export const Dropdown: React.FC<DropdownProps> = ({ name, placeholder, labelText
 					{isOpen && <div className="fixed cursor-pointer inset-0 h-full w-full z-10" aria-hidden="true" onClick={() => setIsOpen(false)}></div>}
 
 					{/* Label */}
-					{labelText && <Label name={name} labelText={labelText}/>}
+					{labelText && <Label name={name} labelText={labelText} inputRequired={inputRequired}/>}
 
 					{/* Input */}
 					<button
@@ -69,7 +69,7 @@ export const Dropdown: React.FC<DropdownProps> = ({ name, placeholder, labelText
 										props.form.setFieldValue(props.field.name, option.value)
 										setIsOpen(false)
 									}}
-									className={`flex items-center hover:text-blue-500 w-full px-5 py-2 ${props.field.value === option.value ? "text-blue-500" : "text-gray-700"}`}
+									className={`flex items-center hover:text-red-500 w-full px-5 py-2 ${props.field.value === option.value ? "text-red-500" : "text-gray-700"}`}
 								>
 									{option.icon && <Icon icon={option.icon} className="mr-2"/>}
 									<span className="truncate pr-1">{option.label}</span>
