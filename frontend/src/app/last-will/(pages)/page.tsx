2e27d22f165b48b2f434db76301c6b1fdb69f817
LarsFlieger <a.purenetwork@gmail.com>
2023-05-30 16:25:57 +0200

    chore(last-will-landing): changed clients grid

--- frontend/src/app/last-will/(pages)/page.tsx
+++ frontend/src/app/last-will/(pages)/page.tsx
@@ -97,7 +97,7 @@ const Home: React.FC = () => {
 				{/* Section: Step by step tutorial end */}
 
 				{/* Section: Clients */}
-				<div className="container mb-5 grid gap-2 md:mb-10 md:grid-cols-2 md:gap-5 lg:grid-cols-4">
+				<div className="container mb-5 grid gap-2 md:mb-10 md:grid-cols-2 md:gap-5 2xl:grid-cols-4">
 					{/* Headline */}
 					<Headline level={2}>Das sagen unsere zufriedenen Kunden</Headline>
 
