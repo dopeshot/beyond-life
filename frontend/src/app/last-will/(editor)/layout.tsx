0de156dd8cdbe484530a422ff694a159bf1f2063
TjWaaaa <timowaldherr98@gmail.com>
2023-05-31 10:39:23 +0200

    fix: remove metadata from use client component

--- frontend/src/app/last-will/(editor)/layout.tsx
+++ frontend/src/app/last-will/(editor)/layout.tsx
@@ -4,11 +4,6 @@ import { GlobalFooter } from '../../../components/Navbar/GlobalFooter/GlobalFoot
 import { ModuleNavbar } from '../../../components/Navbar/ModuleNavbar/ModuleNavbar'
 import { Sidebar } from '../../../components/Navbar/Sidebar/Sidebar'
 
-export const metadata = {
-	title: 'Beyond Life',
-	description: 'Handle your death.',
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	const path = usePathname()
 
