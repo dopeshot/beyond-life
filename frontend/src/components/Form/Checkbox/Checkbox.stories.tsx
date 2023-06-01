546265fb89b579875601a3294a3c46112bed59b3
LarsFlieger <a.purenetwork@gmail.com>
2023-06-01 16:51:16 +0200

    chore(Checkbox): restructured storybook to follow pattern

--- frontend/src/components/Form/Checkbox/Checkbox.stories.tsx
+++ frontend/src/components/Form/Checkbox/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from '@storybook/addon-actions'
-import { Meta, StoryFn } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import { Form, Formik } from 'formik'
 import * as yup from 'yup'
 import { Checkbox, CheckboxProps } from './Checkbox'
@@ -10,43 +10,58 @@ const meta: Meta<typeof Checkbox> = {
 }
 export default meta
 
-const Template: StoryFn<CheckboxProps> = (args) => (
-	<Formik
-		initialValues={{
-			checkbox: [],
-		}}
-		onSubmit={(values) => {
-			action(`form submitted`)(values)
-		}}
-		validationSchema={yup.object().shape({
-			checkbox: yup.array().of(yup.number().min(2)),
-		})}
-	>
-		<Form>
-			<Checkbox {...args} />
-			<button type="submit">Submit</button>
-		</Form>
-	</Formik>
-)
-export const Default = Template.bind({})
-Default.args = {
-	name: 'checkbox',
-	labelText: 'What food do you like?',
-	options: [
-		{ id: 1, label: 'Icecream', icon: 'icecream' },
-		{ id: 2, label: 'Cake', icon: 'cake' },
-		{ id: 3, label: 'Egg', icon: 'egg' },
-	],
+const Template: StoryObj<CheckboxProps> = {
+	render: (args) => {
+		return (
+			<Formik
+				initialValues={{
+					checkbox: [],
+				}}
+				onSubmit={(values) => {
+					action(`form submitted`)(values)
+				}}
+				validationSchema={yup.object().shape({
+					checkbox: yup.array().of(yup.number().min(2)),
+				})}
+			>
+				{(formik) => (
+					<Form>
+						<Checkbox {...args} />
+						<button disabled={!formik.isValid} type="submit">
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
+		)
+	},
 }
 
-export const Required = Template.bind({})
-Required.args = {
-	...Default.args,
-	labelRequired: true,
+export const Default = {
+	...Template,
+	args: {
+		name: 'checkbox',
+		labelText: 'What food do you like?',
+		options: [
+			{ id: 1, label: 'Icecream', icon: 'icecream' },
+			{ id: 2, label: 'Cake', icon: 'cake' },
+			{ id: 3, label: 'Egg', icon: 'egg' },
+		],
+	},
 }
 
-export const HelperText = Template.bind({})
-HelperText.args = {
-	...Default.args,
-	helperText: 'Please select all that apply.',
+export const Required = {
+	...Template,
+	args: {
+		...Default.args,
+		labelRequired: true,
+	},
+}
+
+export const HelperText = {
+	...Template,
+	args: {
+		...Default.args,
+		helperText: 'Please select all that apply.',
+	},
 }
