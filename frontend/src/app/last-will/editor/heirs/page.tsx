af1858c4db299f151d1ad84c2b50ff9858670782
LarsFlieger <a.purenetwork@gmail.com>
2023-06-14 18:39:55 +0200

    feat: add logic for handling array forms

--- frontend/src/app/last-will/editor/heirs/page.tsx
+++ frontend/src/app/last-will/editor/heirs/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Form, Formik } from 'formik'
-import { useEffect } from 'react'
+import { FieldArray, Form, Formik, FormikState } from 'formik'
+import { useEffect, useState } from 'react'
 import * as Yup from 'yup'
 import { Button } from '../../../../components/ButtonsAndLinks/Button/Button'
 import { Route } from '../../../../components/ButtonsAndLinks/Route/Route'
@@ -11,22 +11,41 @@ import { useLastWillContext } from '../../../../store/last-will/LastWillContext'
 import { SidebarPages } from '../../../../types/sidebar'
 
 type HeirsForm = {
-	heirs: string
+	persons: {
+		firstName: string
+		lastName: string
+	}[]
 }
 
 /**
  * Heirs Page
  */
 const Heirs = () => {
-	const initialValues = {
-		heirs: 'test',
+	const initialValues: HeirsForm = {
+		persons: [
+			{
+				firstName: 'Michael',
+				lastName: 'Müller',
+			},
+		],
 	}
+
+	const [DEBUG_submittedState, setDEBUG_submittedState] = useState<any>({})
+
 	const validationSchema: Yup.ObjectSchema<HeirsForm> = Yup.object({
-		heirs: Yup.string().required('Bitte geben Sie einen Namen ein.'),
+		persons: Yup.array()
+			.required("You can't have an empty list of persons")
+			.of(
+				Yup.object().shape({
+					firstName: Yup.string().required('Required'),
+					lastName: Yup.string().required('Required'),
+				})
+			),
 	})
 
 	const submitHeirs = (values: HeirsForm) => {
 		console.log('values:', values)
+		setDEBUG_submittedState(values)
 	}
 
 	const { services } = useLastWillContext()
@@ -40,13 +59,31 @@ const Heirs = () => {
 			<Headline>Erben</Headline>
 			<Formik onSubmit={submitHeirs} validationSchema={validationSchema} initialValues={initialValues}>
 				<Form>
-					<TextInput name="heirs" labelText="Erben" inputRequired />
+					<FieldArray name="persons">
+						{(props) => (
+							<div>
+								{(props.form as FormikState<HeirsForm>).values.persons.map((person, index) => (
+									<div key={index}>
+										<TextInput name={`persons.${index}.firstName`} labelText="Vorname" inputRequired />
+										<TextInput name={`persons.${index}.lastName`} labelText="Nachname" inputRequired />
+										<Button type="button" kind="secondary" onClick={() => props.remove(index)}>
+											Remove
+										</Button>
+									</div>
+								))}
+								<Button type="button" kind="primary" onClick={() => props.push({ firstName: '', lastName: '' })}>
+									Add
+								</Button>
+							</div>
+						)}
+					</FieldArray>
 					<Button kind="primary" type="submit">
 						Submit
 					</Button>
 				</Form>
 			</Formik>
 
+			<p>{JSON.stringify(DEBUG_submittedState)}</p>
 			<Route href={routes.lastWill.inheritance('1')}>
 				<div>Next</div>
 			</Route>
