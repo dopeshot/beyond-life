820202aa6bd821ed669292256279365c4fdc9378
TjWaaaa <timowaldherr98@gmail.com>
2023-06-19 12:31:07 +0200

    feat: basic layout for final page

--- frontend/src/app/last-will/editor/final/page.tsx
+++ frontend/src/app/last-will/editor/final/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { useEffect } from 'react'
-import { Route } from '../../../../components/ButtonsAndLinks/Route/Route'
+import { FormStepsButtons } from '../../../../components/Form/FormStepsButtons/FormStepsButtons'
 import { Headline } from '../../../../components/Headline/Headline'
-import { routes } from '../../../../services/routes/routes'
+import { Icon } from '../../../../components/Icon/Icon'
 import { useLastWillContext } from '../../../../store/last-will/LastWillContext'
 import { SidebarPages } from '../../../../types/sidebar'
 
@@ -17,9 +17,30 @@ const Final = () => {
 	}, [services])
 
 	return (
-		<div className="container mt-5">
-			<Headline>Abschreiben</Headline>
-			<Route href={routes.lastWill.testator('1')}>Final page</Route>
+		<div className="container mt-5 flex flex-1 flex-col">
+			<Headline className="md:mb-8">Zusammenfassung</Headline>
+			<div className="mt-2 flex-1 rounded-xl border-2 border-gray-100 px-4 py-3 md:px-8 md:py-6">
+				<div className="flex">
+					<Icon icon="edit" className="mr-2" />
+					<Headline level={3} size="text-lg">
+						Vorlage zum Abschreiben
+					</Headline>
+				</div>
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+				dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+				clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+				consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+				diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+				takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+				diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+				accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+				dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+				illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+				luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+			</div>
+
+			{/* Form Steps Buttons */}
+			<FormStepsButtons previousOnClick={() => {}} loading={false} />
 		</div>
 	)
 }
