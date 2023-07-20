9d1ececed1cf0141a5fccb1ce065ef7fa40864da
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-07-20 11:16:19 +0200

    feat(mail): add db migration

--- db/migrations/1688321045-add-mail-events/UP.sql
+++ db/migrations/1688321045-add-mail-events/UP.sql
@@ -1,6 +1,7 @@
 -- UP migration file for migration 1688321045-add-mail-events 
 
 CREATE TABLE mail_events(
+    pk_mail_event_id INT GENERATED ALWAYS AS IDENTITY,
     schedulet_at TIMESTAMPTZ NOT NULL,
     -- this is the most simple way to store it
     content JSON NOT NULL,
