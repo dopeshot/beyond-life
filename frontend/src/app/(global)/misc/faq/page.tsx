c0db87343f0b41efd74e86a21eff1e3e1c730eff
Marvin Pfau <marvin.pfau@gmx.de>
2023-07-21 17:50:44 +0200

    Add more content

--- frontend/src/app/(global)/misc/faq/page.tsx
+++ frontend/src/app/(global)/misc/faq/page.tsx
@@ -28,9 +28,9 @@ export const FAQContent = [
 		answer: (
 			<>
 				<p>
-					Die Erstellung eines Testaments mit "Siebtes Leben" bietet eine intuitive und strukturierte Herangehensweise,
-					die Ihnen dabei hilft, alle relevanten Aspekte zu bedenken. Unser spezifischer Fragebogen führt Sie durch den
-					Prozess und hilft, Unklarheiten oder Vergessenes zu vermeiden.
+					Die Erstellung eines Testaments mit &quot;Siebtes Leben&quot; bietet eine intuitive und strukturierte
+					Herangehensweise, die Ihnen dabei hilft, alle relevanten Aspekte zu bedenken. Unser spezifischer Fragebogen
+					führt Sie durch den Prozess und hilft, Unklarheiten oder Vergessenes zu vermeiden.
 				</p>
 			</>
 		),
@@ -42,13 +42,74 @@ export const FAQContent = [
 			<>
 				<p>
 					Ein privates Testament muss der Erblasser handschriftlich, eigenhändig und lesbar verfassen. Das Testament
-					muss das Datum und den Ort, den vollen Namen und die Unterschrift enthalten.
+					muss das Datum und den Ort, den vollen Namen und die Unterschrift enthalten. Siebtes Leben generiert Ihnen ein
+					fertiges Dokument, welches Sie nur noch abschreiben und unterschreiben müssen.
 				</p>
 			</>
 		),
 	},
 	{
 		slug: '4',
+		question: 'Was ist der Unterschied zwischen einem öffentlichen und einem handschriftlichen Testament?',
+		answer: (
+			<>
+				<p className='mb-4'>
+					Ein handschriftliches Testament ist eine Form des Testaments, die vom Erblasser persönlich und handschriftlich
+					verfasst und unterschrieben wird. Es erfordert keine Beglaubigung oder Beurkundung durch einen Notar und kann
+					jederzeit erstellt werden.
+				</p>
+				<p>
+					Ein öffentliches Testament hingegen wird von einem Notar erstellt, nachdem der Erblasser ihm seinen letzten
+					Willen mündlich oder in einer schriftlichen Erklärung mitgeteilt hat. Der Notar verfasst das Testament dann
+					und liest es dem Erblasser vor. Nach dessen Zustimmung unterschreiben sowohl der Erblasser als auch der Notar
+					das Dokument. Das öffentliche Testament ist rechtssicher, da es vom Notar beurkundet wird, aber es fallen
+					Notargebühren an.
+				</p>
+			</>
+		),
+	},
+	{
+		slug: '5',
+		question: 'Warum muss ich mich registrieren?',
+		answer: (
+			<>
+				<p>
+					Die Registrierung auf &quot;Siebtes Leben&quot; ermöglicht es uns, Ihnen eine personalisierte und sichere
+					Erfahrung zu bieten. Mit einem registrierten Konto können Sie Ihren Fortschritt speichern und bei Bedarf
+					darauf zurückkommen. Außerdem hilft es uns, Ihre Daten sicher zu speichern und zu verwalten, um Ihnen den
+					bestmöglichen Service zu bieten. Ihre Privatsphäre ist uns sehr wichtig und wir setzen strenge
+					Sicherheitsmaßnahmen ein, um Ihre persönlichen Informationen zu schützen.
+				</p>
+			</>
+		),
+	},
+	{
+		slug: '6',
+		question: 'Sind meine persönlichen Daten sicher?',
+		answer: (
+			<>
+				<p>
+					Ihre Privatsphäre ist uns sehr wichtig und wir setzen strenge Sicherheitsmaßnahmen ein, um Ihre persönlichen
+					Informationen zu schützen. Wir speichern Ihre Daten auf einem sicheren Server und geben sie nicht an Dritte
+					weiter. Weitere Informationen finden Sie in unserer Datenschutzerklärung. Die Verbindung ist durch die
+					modernste HTTPS Verbindung gesichert.
+				</p>
+			</>
+		),
+	},
+	{
+		slug: '7',
+		question: 'Was passiert, wenn ich kein Testament erstelle?',
+		answer: (
+			<>
+				<p>
+					Wenn Sie kein Testament erstellen, wird Ihr Vermögen nach den gesetzlichen Erbfolgeregeln verteilt. Dies kann zu Streitigkeiten unter den Erben führen und dazu führen, dass Ihr Vermögen nicht nach Ihren Wünschen verteilt wird. Wenn Sie keine Angehörigen haben, wird Ihr Vermögen an den Staat fallen.
+				</p>
+			</>
+		),
+	},
+	{
+		slug: '8',
 		question: 'Was ist der Unterschied zwischen einem öffentlichen und einem handschriftlichen Testament?',
 		answer: (
 			<>
@@ -64,7 +125,6 @@ export const FAQContent = [
 			</>
 		),
 	},
-	
 ]
 
 const FAQOverview = () => {
