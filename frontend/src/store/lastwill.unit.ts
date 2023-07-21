c6dbfe27877122cf8cb178610e3e4790b71780f7
LarsFlieger <a.purenetwork@gmail.com>
2023-07-21 18:35:09 +0200

    feat: add negative test for progress keys

--- frontend/src/store/lastwill.unit.ts
+++ frontend/src/store/lastwill.unit.ts
@@ -15,7 +15,19 @@ describe('should add progress key to global store', () => {
 		const action = setProgressKeys(SidebarPages.TESTATOR)
 		const newState = lastWillReducer(initialState, action)
 
+		// Ensure that the new state has the progress key
 		expect(newState.progressKeys).to.have.lengthOf(1)
 		expect(newState.progressKeys[0]).to.equal(SidebarPages.TESTATOR)
+	})
+
+	it('should not modify state when an unknown action type is provided', () => {
+		// Ensure that the initial state is empty
+		expect(initialState.progressKeys).to.have.lengthOf(0)
+
+		const unknownAction = { type: 'UNKNOWN_ACTION', payload: {} }
+		const newState = lastWillReducer(initialState, unknownAction)
+
+		// Ensure that the state remains unchanged
+		expect(newState).to.deep.equal(initialState)
 	})
 })
