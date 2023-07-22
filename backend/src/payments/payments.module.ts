b6c6715107e57cdee9a8d9884b39a3b672aec56c
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-23 00:20:46 +0200

    add: stripe service

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from '../auth/auth.module'
 import { DbModule } from '../db/db.module'
 import { SharedModule } from '../shared/shared.module'
 import { PaymentsController } from './payments.controller'
 import { PaymentsService } from './services/payments.service'
+import { StripeService } from './services/stripe.service'
 
 @Module({
-  imports: [DbModule, SharedModule, AuthModule],
+  imports: [DbModule, SharedModule],
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, StripeService],
 })
 export class PaymentsModule {}
---
+++ backend/src/payments/services/stripe.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, Logger, ServiceUnavailableException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Stripe } from 'stripe'
+
+@Injectable()
+export class StripeService {
+  private logger = new Logger(StripeService.name)
+  private stripe: Stripe
+  constructor(private configService: ConfigService) {
+    this.stripe = new Stripe(configService.get('STRIPE_SECRET_KEY'), {
+      apiVersion: '2022-11-15',
+    })
+  }
+
+  async customer_create(name: string, email: string) {
+    try {
+      const customer = await this.stripe.customers.create({
+        name,
+        email,
+      })
+      return customer.id
+    } catch (error) {
+      this.logger.log(error)
+      throw new ServiceUnavailableException('Payment service is unavailable')
+    }
+  }
+
+  async customer_retrieve(customerId: string) {
+    try {
+      return (await this.stripe.customers.retrieve(customerId)).id
+    } catch (error) {
+      this.logger.log(error)
+      throw new ServiceUnavailableException('Payment service is unavailable')
+    }
+  }
+
+  async paymentIntent_create(
+    amount: number,
+    customerId: string,
+    payment_method: string,
+  ) {
+    try {
+      return await this.stripe.paymentIntents.create({
+        customer: customerId,
+        amount,
+        payment_method,
+        currency: 'eur',
+        confirm: true,
+      })
+    } catch (error) {
+      this.logger.log(error)
+      throw new ServiceUnavailableException('Payment service is unavailable')
+    }
+  }
+}
