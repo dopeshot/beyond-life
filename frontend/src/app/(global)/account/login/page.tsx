d658c5dea9153e95e1b818400bee3b71765f37b8
TjWaaaa <timowaldherr98@gmail.com>
2023-07-22 17:11:38 +0200

    style: reformat files

--- frontend/src/app/(global)/account/login/page.tsx
+++ frontend/src/app/(global)/account/login/page.tsx
@@ -4,28 +4,30 @@ import { Headline } from '../../../../components/Headline/Headline'
 import { routes } from '../../../../services/routes/routes'
 
 export const metadata = {
-    title: 'Login | Siebtes Leben'
+	title: 'Login | Siebtes Leben',
 }
 
 /**
  * Login Page
  */
 const Login = () => {
-    return (
-        <div className="container mt-10 md:mt-32 md:w-1/2 2xl:w-1/3">
-            <div className="text-center mb-5 md:mb-10">
-                <Headline>Willkommen zurück!</Headline>
-                <p>In ihren Account einloggen.</p>
-            </div>
+	return (
+		<div className="container mt-10 md:mt-32 md:w-1/2 2xl:w-1/3">
+			<div className="mb-5 text-center md:mb-10">
+				<Headline>Willkommen zurück!</Headline>
+				<p>In ihren Account einloggen.</p>
+			</div>
 
-            <AccountForm type="login" />
+			<AccountForm type="login" />
 
-            <div className="flex justify-center text-sm items-baseline gap-2">
-                <p className="font-medium text-gray-500">Noch keinen Account?</p>
-                <Route href={routes.account.register} kind="tertiary">Jetzt Registrieren</Route>
-            </div>
-        </div>
-    )
+			<div className="flex items-baseline justify-center gap-2 text-sm">
+				<p className="font-medium text-gray-500">Noch keinen Account?</p>
+				<Route href={routes.account.register} kind="tertiary">
+					Jetzt Registrieren
+				</Route>
+			</div>
+		</div>
+	)
 }
 
 export default Login
--- frontend/src/app/(global)/account/register/page.tsx
+++ frontend/src/app/(global)/account/register/page.tsx
@@ -4,28 +4,30 @@ import { Headline } from '../../../../components/Headline/Headline'
 import { routes } from '../../../../services/routes/routes'
 
 export const metadata = {
-    title: 'Register | Siebtes Leben'
+	title: 'Register | Siebtes Leben',
 }
 
 /**
  * Register Page
  */
 const Register = () => {
-    return (
-        <div className="container mt-10 md:mt-32 md:w-1/2 2xl:w-1/3">
-            <div className="text-center mb-5 md:mb-10">
-                <Headline>Jetzt loslegen!</Headline>
-                <p>Einen neuen Account anlegen.</p>
-            </div>
+	return (
+		<div className="container mt-10 md:mt-32 md:w-1/2 2xl:w-1/3">
+			<div className="mb-5 text-center md:mb-10">
+				<Headline>Jetzt loslegen!</Headline>
+				<p>Einen neuen Account anlegen.</p>
+			</div>
 
-            <AccountForm type="register" />
+			<AccountForm type="register" />
 
-            <div className="flex justify-center text-sm items-baseline gap-2">
-                <p className="font-medium text-gray-500">Sie haben schon einen Account?</p>
-                <Route href={routes.account.login} kind="tertiary">Jetzt Einloggen</Route>
-            </div>
-        </div>
-    )
+			<div className="flex items-baseline justify-center gap-2 text-sm">
+				<p className="font-medium text-gray-500">Sie haben schon einen Account?</p>
+				<Route href={routes.account.login} kind="tertiary">
+					Jetzt Einloggen
+				</Route>
+			</div>
+		</div>
+	)
 }
 
 export default Register
