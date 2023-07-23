2c0b57922d1f72dc974d80d6ba2e0678a2ee8a9a
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-23 17:45:53 +0200

    add: disable body Parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bodyParser: false,
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
