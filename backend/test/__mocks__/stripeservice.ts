618e030b5f8ff977da932a16c9d895148b9f190e
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-24 13:32:12 +0200

    add: replace mock with new workflow mock

--- backend/test/__mocks__/stripeservice.ts
+++ backend/test/__mocks__/stripeservice.ts
@@ -1,16 +1,35 @@
+import { UnauthorizedException } from '@nestjs/common'
+
 export class MockStripeService {
-  async customer_create(name, email) {
-    return 'cus_123'
-  }
-
-  async customer_retrieve(customerId) {
-    return customerId
-  }
-
-  async paymentIntent_create(amount, customerId, payment_method) {
+  async checkout_session_create(plan: string, price: string, userId: string) {
     return {
-      status: 'succeeded',
-      amount_received: amount,
+      id: 'cs_test_...',
+      object: 'checkout.session',
+      price,
+      metadata: {
+        plan: 'single',
+        userId,
+      },
+      mode: 'payment',
+      payment_method_types: ['card', 'paypal', 'klarna'],
+      payment_status: 'unpaid',
+      status: 'open',
+    }
+  }
+  async webhook_constructEvent(payload: any, signature: string) {
+    if (signature !== 'valid_signature')
+      throw new UnauthorizedException('Invalid signature')
+    return {
+      type: 'checkout.session.completed',
+      data: {
+        object: {
+          payment_status: 'paid',
+          metadata: {
+            plan: 'something',
+            userId: '64baa1b699fd7a8584b469d6',
+          },
+        },
+      },
     }
   }
 }
