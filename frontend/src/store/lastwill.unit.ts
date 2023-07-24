d9d4e222d5adc4be0f4f7aa3ad4e889734786795
LarsFlieger <a.purenetwork@gmail.com>
2023-07-24 16:59:07 +0200

    feat: add tests for marriage global store

--- frontend/src/store/lastwill.unit.ts
+++ frontend/src/store/lastwill.unit.ts
@@ -1,3 +1,4 @@
+import { MarriageFormPayload } from '../app/(dynamic)/last-will/editor/marriage/page'
 import { SidebarPages } from '../types/sidebar'
 import {
 	LastWillState,
@@ -5,6 +6,7 @@ import {
 	initialState,
 	lastWillReducer,
 	setInheritance,
+	setMarriage,
 	setProgressKeys,
 	setTestator,
 } from './lastwill'
@@ -112,6 +114,70 @@ describe('lastWillSlice', () => {
 			expect(newState.data.testator).to.deep.equal(testator)
 		})
 	})
+
+	describe('marriage', () => {
+		const action = {
+			name: 'John Doe',
+			gender: 'male',
+			birthDate: '1990-01-01',
+			birthPlace: 'New York',
+			moreInfos: ['isHandicapped', 'isBerlinWill'],
+			street: 'Main St.',
+			houseNumber: '123',
+			zipCode: '12345',
+			city: 'New York',
+			isPartnerGermanCitizenship: ['isPartnerGermanCitizenship'],
+			matrimonialProperty: 'communityOfGain',
+			relationshipStatus: 'married',
+		} as MarriageFormPayload
+
+		const expectedPartnerObject = {
+			type: 'partner',
+			name: 'John Doe',
+			gender: 'male',
+			birthDate: '1990-01-01',
+			birthPlace: 'New York',
+			isHandicapped: true,
+			isInsolvent: false,
+			street: 'Main St.',
+			houseNumber: '123',
+			zipCode: '12345',
+			city: 'New York',
+		}
+
+		beforeEach(() => {
+			// Ensure that the initial state is empty
+			expect(initialStateTesting.data.heirs).to.have.lengthOf(0)
+		})
+
+		it(`it should set partner data when ${setMarriage} is called`, () => {
+			const newState = lastWillReducer(initialStateTesting, setMarriage(action))
+
+			const partnerWithoutId = Cypress._.omit(newState.data.heirs[0], 'id')
+			expect(partnerWithoutId).to.be.deep.equal(expectedPartnerObject)
+		})
+
+		it('should set relationshipStatus to married when setMarriage is called', () => {
+			const newState = lastWillReducer(initialStateTesting, setMarriage(action))
+			expect(newState.data.testator.relationshipStatus).to.equal('married')
+		})
+
+		it('should set isBerlinWill to true when setMarriage is called with isBerlinWill in moreInfos', () => {
+			const newState = lastWillReducer(initialStateTesting, setMarriage(action))
+			expect(newState.data.common.isBerlinWill).to.be.true
+		})
+
+		it('should set isPartnerGermanCitizenship to true when setMarriage is called with isPartnerGermanCitizenship', () => {
+			const newState = lastWillReducer(initialStateTesting, setMarriage(action))
+			expect(newState.data.common.isPartnerGermanCitizenship).to.be.true
+		})
+
+		it('should set matrimonialProperty to communityOfGain when setMarriage is called with matrimonialProperty as communityOfGain', () => {
+			const newState = lastWillReducer(initialStateTesting, setMarriage(action))
+			expect(newState.data.common.matrimonialProperty).to.equal('communityOfGain')
+		})
+	})
+
 	describe('progress keys', () => {
 		it(`should add a progress key when ${setProgressKeys} is called`, () => {
 			// Ensure that the initial state is empty
