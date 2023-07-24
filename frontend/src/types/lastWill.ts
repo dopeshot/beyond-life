cbfee899c73f1ec2f998d3986e517b51f32fda8b
LarsFlieger <a.purenetwork@gmail.com>
2023-07-24 11:37:38 +0200

    feat: add person type and testator type

--- frontend/src/types/lastWill.ts
+++ frontend/src/types/lastWill.ts
@@ -1,3 +1,25 @@
+export type Testator = {
+	relationshipStatus?: RelationshipStatus
+} & Omit<Person, 'type' | 'percentage' | 'itemIds' | 'id' | 'childType'>
+
+type Person = {
+	type: PersonType
+	name: string
+	gender?: Gender
+	birthDate?: string
+	birthPlace?: string
+	isHandicapped?: boolean
+	isInsolvent?: boolean
+
+	// Succession
+	percentage?: number
+	itemIds?: number[]
+
+	// Heirs
+	childType?: 'natural' | 'adopted' | 'step'
+} & Address &
+	Id
+
 export type FinancialAsset = {
 	where?: string
 	amount?: number
@@ -12,3 +34,12 @@ export type Item = {
 type Id = {
 	id: string
 }
+type Address = {
+	street?: string
+	houseNumber?: string
+	zipCode?: string
+	city?: string
+}
+type PersonType = 'mother' | 'father' | 'child' | 'siblings' | 'other' | 'organisation'
+type Gender = 'male' | 'female' | 'divers'
+type RelationshipStatus = 'married' | 'divorced' | 'widowed' | 'unmarried'
