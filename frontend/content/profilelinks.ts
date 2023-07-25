0c6536b0489c81e0149eb1cf638c311558b8bfeb
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-07-25 09:42:50 +0200

    feat(codecov): implement PR feedback

--- frontend/content/profilelinks.ts
+++ frontend/content/profilelinks.ts
@@ -16,7 +16,7 @@ export const profileLinks = [
 		icon: 'logout',
 		href: '',
 		onClick: () => console.log('logout'),
-	} as any,
+	},
 	// satisfies would be a better way of solving this...
 	// However as of now using satisfies with a proper type is not possible here because of babel during testing
 	// Also see https://github.com/vercel/next.js/pull/51962 ("We have moved on to SWC so why should we care")
@@ -24,5 +24,5 @@ export const profileLinks = [
 	name: string
 	icon: 'history' | 'settings' | 'history_edu'
 	href: string
-	onclick?: () => void
+	onClick?: () => void
 }[]
