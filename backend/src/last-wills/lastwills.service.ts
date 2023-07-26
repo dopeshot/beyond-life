284ac3febe47e82f958942cb267d2ee7122fa878
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-26 21:19:04 +0200

    add: function skeleton for lastwill

--- backend/src/last-wills/lastwills.service.ts
+++ backend/src/last-wills/lastwills.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common'
+import { ReturnModelType } from '@typegoose/typegoose'
+import { ObjectId } from 'mongoose'
 import { CreateLastWillDto } from './dto/create-lastwill.dto'
 import { UpdateLastWillDto } from './dto/update-lastwill.dto'
+import { LastWill } from './entities/lastwill.entity'
 
 @Injectable()
 export class LastWillsService {
-  createOne(createLastWillDto: CreateLastWillDto) {
+  constructor(
+    private readonly lastWillModel: ReturnModelType<typeof LastWill>,
+  ) {}
+  async createOne(createLastWillDto: CreateLastWillDto, userId: ObjectId) {
+    // TODO: not possible if exceeds allowed by plan
     return 'This action adds a new lastWill'
   }
 
-  findAllMetadataByUser() {
+  async findAllMetadataByUser(userId: ObjectId) {
     return `This action returns all lastWills`
   }
 
-  findFullById(id: number) {
+  async findFullById(id: string, userId: ObjectId) {
+    return `This action returns a #${id} lastWill`
+  }
+  async getFullTextLastWill(id: string, userId: ObjectId) {
     return `This action returns a #${id} lastWill`
   }
 
-  updateOneById(id: number, updateLastWillDto: UpdateLastWillDto) {
+  async updateOneById(
+    id: string,
+    userId: ObjectId,
+    updateLastWillDto: UpdateLastWillDto,
+  ) {
     return `This action updates a #${id} lastWill`
   }
 
-  deleteOneById(id: number) {
+  async deleteOneById(id: string, userId: ObjectId) {
+    // TODO: delete only if user is matching aswell
+
     return `This action removes a #${id} lastWill`
   }
 }
