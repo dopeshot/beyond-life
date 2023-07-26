7596918556e1858f06fdefd7d06894f97579a077
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-26 21:19:18 +0200

    fix: missing stuff

--- backend/src/last-wills/lastwills.module.ts
+++ backend/src/last-wills/lastwills.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from '../auth/auth.module'
+import { DbModule } from '../db/db.module'
 import { ProfileModule } from '../profile/profile.module'
 import { SharedModule } from '../shared/shared.module'
 import { LastWillsController } from './lastwills.controller'
 import { LastWillsService } from './lastwills.service'
 
 @Module({
-  imports: [ProfileModule, SharedModule, AuthModule],
+  imports: [ProfileModule, SharedModule, AuthModule, DbModule],
   controllers: [LastWillsController],
   providers: [LastWillsService],
 })
