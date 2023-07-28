0c2f32ef563536b9501d04b205e016c0bc10367c
LarsFlieger <a.purenetwork@gmail.com>
2023-07-28 17:40:02 +0200

    refactor: layout auth

--- frontend/src/app/(dynamic)/layout.tsx
+++ frontend/src/app/(dynamic)/layout.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { useEffect } from 'react'
 import { Provider } from 'react-redux'
-import { LOCAL_STORAGE_KEY } from '../../services/auth/session'
+import { hasSession } from '../../services/auth/session'
 import { login, logout, refreshToken } from '../../store/auth/auth'
 import { useAppDispatch } from '../../store/hooks'
 import { store } from '../../store/store'
+import { SessionData } from '../../types/auth'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
@@ -24,15 +25,14 @@ const Auth: React.FC<AuthProps> = ({ children }) => {
 	useEffect(() => {
 		const checkAuthenticated = async () => {
 			// TODO: Check when this is called, and ensure it's only called once
-			await dispatch(refreshToken())
-			const sessionDataLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY)
 
-			if (!sessionDataLocalStorage) {
+			if (!hasSession()) {
 				dispatch(logout())
 				return null
 			}
 
-			dispatch(login(JSON.parse(sessionDataLocalStorage)))
+			const sessionPayload = await dispatch(refreshToken())
+			dispatch(login(sessionPayload.payload as SessionData))
 		}
 		checkAuthenticated()
 	}, [dispatch])
