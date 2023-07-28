3e5abc6e184a1de96a04a8be6b25aee8e5f82424
LarsFlieger <a.purenetwork@gmail.com>
2023-07-28 14:49:15 +0200

    refactor: remove old store

--- frontend/src/app/(dynamic)/last-will/editor/layout.tsx
+++ frontend/src/app/(dynamic)/last-will/editor/layout.tsx
@@ -7,7 +7,6 @@ import { NavbarLogo } from '../../../../components/Navbar/NavbarLogo/NavbarLogo'
 import { MobileSidebar } from '../../../../components/Navbar/Sidebar/MobileSidebar/MobileSidebar'
 import { Sidebar } from '../../../../components/Navbar/Sidebar/Sidebar'
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks'
-import { LastWillContextProvider } from '../../../../store/last-will/LastWillContext'
 import { fetchLastWillState, resetLastWill } from '../../../../store/lastwill'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -34,21 +33,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			{!isInitialized ? (
 				<p>init...</p>
 			) : (
-				<LastWillContextProvider>
-					<div className={`flex h-screen min-h-screen w-full overflow-y-scroll sm:flex-col lg:flex-row`}>
-						<Sidebar path={path} />
-						<div className="flex h-fit min-h-screen flex-grow flex-col">
-							<Navbar background={false}>
-								<div className="mr-5 lg:hidden">
-									<NavbarLogo />
-								</div>
-							</Navbar>
-							<MobileSidebar path={path} />
-							{children}
-							<GlobalFooter />
-						</div>
+				<div className={`flex h-screen min-h-screen w-full overflow-y-scroll sm:flex-col lg:flex-row`}>
+					<Sidebar path={path} />
+					<div className="flex h-fit min-h-screen flex-grow flex-col">
+						<Navbar background={false}>
+							<div className="mr-5 lg:hidden">
+								<NavbarLogo />
+							</div>
+						</Navbar>
+						<MobileSidebar path={path} />
+						{children}
+						<GlobalFooter />
 					</div>
-				</LastWillContextProvider>
+				</div>
 			)}
 		</>
 	)
