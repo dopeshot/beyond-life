00208357b159d927706359dac31613b732274dc4
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-28 15:19:10 +0200

    add: types

--- backend/src/db/entities/lastwill.entity.ts
+++ backend/src/db/entities/lastwill.entity.ts
@@ -1,174 +1,375 @@
-import { ApiProperty, OmitType } from '@nestjs/swagger'
+import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger'
 import { prop } from '@typegoose/typegoose'
 import { Expose } from 'class-transformer'
 import { ObjectId } from 'mongoose'
+import { User } from './users.entity'
 
-type PersonType =
-  | 'mother'
-  | 'father'
-  | 'child'
-  | 'siblings'
-  | 'other'
-  | 'organisation'
-type Gender = 'male' | 'female' | 'divers'
-type RelationshipStatus = 'married' | 'divorced' | 'widowed' | 'unmarried'
-type MatrimonialProperty = 'communityOfGain' | 'separationOfProperty'
-const MatrimonialPropertyValues = ['communityOfGain', 'separationOfProperty']
+enum PersonType {
+  MOTHER = 'mother',
+  FATHER = 'father',
+  CHILD = 'child',
+  SIBLING = 'siblings',
+  OTHER = 'other',
+  ORGANISATION = 'organisation',
+}
 
+enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  DIVERS = 'divers',
+}
+
+enum RelationshipStatus {
+  MARRIED = 'married',
+  DIVORCED = 'divorced',
+  WIDOWED = 'widowed',
+  UNMARRIED = 'unmarried',
+}
+
+enum MatrimonialProperty {
+  COMMUNITY_OF_GAIN = 'communityOfGain',
+  SEPARATION_OF_PROPERTY = 'separationOfProperty',
+}
+
+enum SidebarPages {
+  TESTATOR = 'testator',
+  MARRIAGE = 'marriage',
+  HEIRS = 'heirs',
+  INHERITANCE = 'inheritance',
+  SUCCESSION = 'succession',
+  FINAL = 'final',
+}
+
+enum ChildType {
+  NATURAL = 'natural',
+  ADOPTED = 'adopted',
+  STEP = 'step',
+}
+
+enum ChildRelationShip {
+  CHILD_TOGETHER = 'childTogether',
+  CHILD_FROM_PARTNER = 'childFromPartner',
+  CHILD_FROM_OTHER = 'childFromOther',
+}
+
+@Expose()
+class ChildInfo {
+  @prop({ required: false, enum: ChildType, type: String })
+  @ApiPropertyOptional({
+    description: 'Child type',
+    example: ChildType[0],
+    enum: ChildType,
+    type: String,
+  })
+  type?: ChildType
+
+  @prop({ required: false, enum: ChildRelationShip, type: String })
+  @ApiPropertyOptional({
+    description: 'Child relationship',
+    example: ChildRelationShip[0],
+    enum: ChildRelationShip,
+    type: String,
+  })
+  relationship?: ChildRelationShip
+}
+@Expose()
 export class Common {
-  @prop({ required: false })
-  @Expose()
-  @ApiProperty({
+  @prop({ required: false, type: Boolean })
+  @ApiPropertyOptional({
     description: 'Is it a Berlin Will',
     example: false,
+    type: Boolean,
   })
   isBerlinWill?: boolean
 
-  @Expose()
-  @prop({ required: false })
-  @ApiProperty({
+  @prop({ required: false, type: Boolean })
+  @ApiPropertyOptional({
     description: 'Does the partner have a german citizenship',
     example: false,
+    type: Boolean,
   })
   isPartnerGermanCitizenship?: boolean
 
-  @prop({ required: false, enum: MatrimonialPropertyValues })
-  @Expose()
-  @ApiProperty({
+  @prop({ required: false, enum: MatrimonialProperty, type: String })
+  @ApiPropertyOptional({
     description: 'How to handle the property',
-    example: 'communityOfGain',
-    enum: MatrimonialPropertyValues,
-    type: MatrimonialPropertyValues,
+    example: MatrimonialProperty[0],
+    enum: MatrimonialProperty,
+    type: String,
   })
   matrimonialProperty?: MatrimonialProperty
-
-  @prop({ required: true, default: [] })
-  @Expose()
-  @ApiProperty({ description: 'Progress keys', example: ['test'] })
-  progressKeys: string[]
 }
 
+@Expose()
 class Address {
-  @prop({ required: false })
-  @Expose()
-  @ApiProperty({ description: 'Street', example: 'maxisstr.' })
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Street',
+    example: 'firebasestr.',
+    type: String,
+  })
   street?: string
 
-  @prop({ required: false })
-  @Expose()
-  // String because house numbers could be like 20a
-  @ApiProperty({ description: 'House number', example: '1' })
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'House number',
+    example: '1a',
+    type: String,
+  })
   houseNumber?: string
 
-  @prop({ required: false })
-  @Expose()
-  @ApiProperty({ description: 'Zip code', example: '12345' })
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Zip code',
+    example: '12345',
+    type: String,
+  })
   zipCode?: string
 
-  @prop({ required: false })
-  @Expose()
-  @ApiProperty({ description: 'City', example: 'Berlin' })
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({ description: 'City', example: 'Berlin', type: String })
   city?: string
 }
 
+@Expose()
 class Person {
+  @prop({ required: true, type: String })
+  @ApiProperty({ description: 'Id', example: '123456789', type: String })
+  id: string
+
+  @prop({ required: true, enum: PersonType, type: String })
+  @ApiProperty({
+    description: 'Persontype',
+    example: PersonType[0],
+    enum: PersonType,
+    type: String,
+  })
   type: PersonType
+
+  @prop({ required: true, type: String })
+  @ApiProperty({ description: 'Name', example: 'Max Gert', type: String })
   name: string
+
+  @prop({ required: false, enum: Gender, type: String })
+  @ApiPropertyOptional({ required: false, enum: Gender, example: Gender[0] })
   gender?: Gender
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Birthdate',
+    example: '01.01.01',
+    type: String,
+  })
   birthDate?: string
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Birthplace',
+    example: 'Berlin',
+    type: String,
+  })
   birthPlace?: string
+
+  @prop({ required: false, type: Boolean })
+  @ApiPropertyOptional({
+    description: 'Is handicapped',
+    example: false,
+    type: Boolean,
+  })
   isHandicapped?: boolean
+
+  @prop({ required: false, type: Boolean })
+  @ApiPropertyOptional({
+    description: 'Is insolvent',
+    example: false,
+    type: Boolean,
+  })
   isInsolvent?: boolean
 
   // Succession
+  @prop({ required: false, type: Number })
+  @ApiPropertyOptional({ description: 'Percentage', example: 50, type: Number })
   percentage?: number
+
+  @prop({ required: false, type: [Number], default: [] })
+  @ApiPropertyOptional({
+    description: 'Item ids',
+    example: [1, 2, 3],
+    type: [Number],
+    isArray: true,
+  })
   itemIds?: number[]
 
   // Heirs
-  childType?: 'natural' | 'adopted' | 'step'
-  id: string
+  @prop({ required: false, type: ChildInfo })
+  @ApiPropertyOptional({
+    description: 'Child Info',
+    type: ChildInfo,
+  })
+  child?: ChildInfo
+
+  @prop({ required: false, type: Address })
+  @ApiPropertyOptional({
+    description: 'Address',
+    type: Address,
+  })
   address?: Address
 }
 
-class Testator extends OmitType(Person, ['type', 'percentage', 'itemIds']) {
+@Expose()
+// TODO: check if OmitType is working
+class Testator extends OmitType(Person, [
+  'type',
+  'percentage',
+  'itemIds',
+  'id',
+  'child',
+]) {
+  @prop({ required: false, enum: RelationshipStatus, type: String })
+  @ApiPropertyOptional({
+    description: 'Relationship status',
+    example: RelationshipStatus[0],
+    enum: RelationshipStatus,
+    type: String,
+  })
   relationshipStatus?: RelationshipStatus
 }
 
+@Expose()
 class Organisation {
-  name?: string
+  @prop({ required: true, type: String })
+  @ApiProperty({ description: 'Id', example: '123456789', type: String })
   id: string
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Name',
+    example: 'Max GmbH',
+    type: String,
+  })
+  name?: string
+
+  @prop({ required: false, type: Address })
+  @ApiPropertyOptional({
+    description: 'Address',
+    type: Address,
+  })
   address?: Address
 }
 
+@Expose()
 class Item {
+  @prop({ required: true, type: String })
+  @ApiProperty({ description: 'Id', example: '123456789', type: String })
+  id: string
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Name',
+    example: 'Max Puppe',
+    type: String,
+  })
   name?: string
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Description',
+    example: 'Eine Hundepuppe',
+    type: String,
+  })
   description?: string
-  id: string
 }
 
+@Expose()
 class FinancialAsset {
-  where?: string
-  amount?: number | string
-  currency?: string
+  @prop({ required: true, type: String })
+  @ApiProperty({ description: 'Id', example: '123456789', type: String })
   id: string
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Where',
+    example: 'PayPal',
+    type: String,
+  })
+  where?: string
+
+  @prop({ required: false, type: Number })
+  @ApiPropertyOptional({ description: 'Amount', example: 420.69, type: Number })
+  amount?: number
+
+  @prop({ required: false, type: String })
+  @ApiPropertyOptional({
+    description: 'Currency',
+    example: 'EUR',
+    type: String,
+  })
+  currency?: string
 }
 
+@Expose()
 export class LastWill {
-  @Expose()
   _id: ObjectId
 
+  @prop({ required: true, type: String, ref: () => User })
+  @ApiProperty({
+    description: 'Account id',
+    example: 'aaaaaaaaaaaaaaaaaaaaaaaa',
+    type: String,
+  })
+  accountId: string
+
   @prop({ required: true, type: Common })
-  @Expose()
   @ApiProperty({
     type: Common,
     description: 'Common data for the will',
-    example: {
-      /* TODO */
-    },
   })
   common: Common
 
   @prop({ required: true, type: Testator })
-  @Expose()
   @ApiProperty({
     type: Testator,
     description: 'Testator data',
-    example: {
-      /* TODO */
-    },
   })
   testator: Testator
 
-  @prop({ required: true, type: [Person], default: [] }) // Frontend always rather haves an empty array than null objects
-  @Expose()
+  // TODO: figure out union array
+  @prop({ required: true, /*type: [any],*/ default: [] })
   @ApiProperty({
-    type: [Person],
+    //type: [Person | Organisation],
     description: 'Heirs',
-    example: {
-      /* TODO */
-    },
+    isArray: true,
+    //anyOf: [Person, Organisation],
   })
   heirs: (Person | Organisation)[]
 
   @prop({ required: true, type: [Item], default: [] })
-  @Expose()
   @ApiProperty({
     type: [Item],
     description: 'Items',
-    example: {
-      /* TODO */
-    },
+    isArray: true,
   })
   items: Item[]
 
   @prop({ required: true, type: [FinancialAsset], default: [] })
-  @Expose()
   @ApiProperty({
     type: [FinancialAsset],
     description: 'Financial assets',
-    example: {
-      /* TODO */
-    },
+    isArray: true,
   })
   financialAssets: FinancialAsset[]
+
+  @prop({ required: true, default: [] })
+  @ApiProperty({
+    description: 'Progress keys',
+    example: SidebarPages[0],
+    enum: SidebarPages,
+    type: String,
+    isArray: true,
+  })
+  progressKeys: SidebarPages[]
+
+  constructor(partial: Partial<LastWill>) {
+    Object.assign(this, partial)
+  }
 }
