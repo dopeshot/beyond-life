672f4738eae1105eeec989c95f35a6e8955a94dd
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-29 01:46:03 +0200

    update: imports and Bad Request

--- backend/src/last-wills/lastwills.module.ts
+++ backend/src/last-wills/lastwills.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from '../auth/auth.module'
 import { DbModule } from '../db/db.module'
-import { ProfileModule } from '../profile/profile.module'
 import { SharedModule } from '../shared/shared.module'
 import { LastWillsController } from './lastwills.controller'
 
 @Module({
-  imports: [ProfileModule, SharedModule, AuthModule, DbModule],
+  imports: [SharedModule, DbModule],
   controllers: [LastWillsController],
   providers: [],
 })
