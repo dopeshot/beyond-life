f64bd928217128e71fb91c42923555dadcc93477
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2023-07-29 20:34:11 +0200

    remove: unsused code + istanbul ignore stripe

--- backend/src/payments/interfaces/payments.ts
+++ backend/src/payments/interfaces/payments.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Expose } from 'class-transformer'
 import { IsEnum } from 'class-validator'
 
 export class PaymentDTO {
@@ -29,24 +28,4 @@ export const paymentPlans: Plans = {
   free: -1,
   single: 1,
   family: 5,
-}
-
-export class PaymentResponse {
-  @ApiProperty({
-    description: 'Payment status',
-    example: 'succeeded',
-  })
-  @Expose()
-  status: string
-
-  @ApiProperty({
-    description: 'Amount received',
-    example: 4900,
-  })
-  @Expose()
-  amount_received: number
-
-  constructor(partial: Partial<PaymentResponse>) {
-    Object.assign(this, partial)
-  }
 }
