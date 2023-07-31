88819a8cc155cfead8c9768a70a826e24a3ea88f
TjWaaaa <timowaldherr98@gmail.com>
2023-07-31 22:20:17 +0200

    fix: change grid sizes

--- frontend/src/app/(dynamic)/last-will/editor/succession/page.tsx
+++ frontend/src/app/(dynamic)/last-will/editor/succession/page.tsx
@@ -171,7 +171,7 @@ const Succession = () => {
 				{({ values, dirty, setFieldValue }: FormikProps<SuccessionFormPayload>) => (
 					<Form>
 						{/* heirs */}
-						<div className="mt-5 grid grid-cols-1 gap-6 md:mt-6 md:grid-cols-2 xl:grid-cols-3">
+						<div className="mt-5 grid grid-cols-1 gap-6 md:mt-6 md:grid-cols-2 2xl:grid-cols-3">
 							{values.heirs.map((heir, index) => (
 								<SuccessionHeir
 									onClick={() => {
