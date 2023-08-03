e3fd1d44adabe1e27f7148d6ae0d41aa9f39c75f
coffemakingtoaster <mh340@hdm-stuttgart.de>
2023-08-03 15:21:13 +0200

    feat(docker compose): add init data

--- .docker/mongodb-init/mongo-init.js
+++ .docker/mongodb-init/mongo-init.js
@@ -1,32 +1,17 @@
-console.log("starting")
-
-db = new Mongo().getDB("testDB");
-
-console.log("running")
-
-db.createCollection('users', { capped: false })
-
-console.log("exporting")
-
-// WeAllLoveDevOps
-db.users.insert([
+db.users.insertOne(
     {
-        "_id": {
-            "$oid": "64cb73dd6e746811d8e5bbde"
+        _id: ObjectId("64cb73dd6e746811d8e5bbde"),
+        email: "devops@totally.rocks",
+        password: "$2b$10$vCZrO9z18.xnQM.6ltSakOZWbNriN9U6983rswwMzyBCwmafQWqGm",
+        createdAt: new Date(),
+        hasVerifiedEmail: false,
+        paymentPlan: "free",
+        checkoutInformation: {
+            status: "free",
+            lastInformationTime: 0
         },
-        "email": "devops@totally.rocks",
-        "password": "$2b$10$vCZrO9z18.xnQM.6ltSakOZWbNriN9U6983rswwMzyBCwmafQWqGm",
-        "createdAt": {
-            "$date": "2023-08-03T09:31:09.315Z"
-        },
-        "hasVerifiedEmail": false,
-        "paymentPlan": "free",
-        "checkoutInformation": {
-            "status": "free",
-            "lastInformationTime": 0
-        },
-        "__v": 0,
-        "lastLogin": {
-            "$date": "2023-08-03T09:31:09.372Z"
-        }
-    }])
+        __v: 0,
+        lastLogin: new Date()
+    }
+)
+
