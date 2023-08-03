a4f359dee8618eaa62f5ae16200adb8979d2cf62
LarsFlieger <a.purenetwork@gmail.com>
2023-08-03 17:25:38 +0200

    feat: add error handling at testator when fetch is rejected

--- frontend/src/app/(dynamic)/last-will/editor/testator/page.tsx
+++ frontend/src/app/(dynamic)/last-will/editor/testator/page.tsx
@@ -65,7 +65,11 @@ const Testator = () => {
 			// Update marriage global state
 			dispatch(setTestator(values))
 
-			await dispatch(sendLastWillState())
+			const response = await dispatch(sendLastWillState())
+			if (response.meta.requestStatus === 'rejected') {
+				return
+				// TODO: Add error handling here
+			}
 
 			// Redirect to previous or next page
 			router.push(href)
