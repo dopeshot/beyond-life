5b028450983e58cbd04abb9ab913e256ebbb4c5e
LarsFlieger <a.purenetwork@gmail.com>
2023-08-04 13:21:15 +0200

    fix: bug with session data failed to set while calling login

--- frontend/src/app/(dynamic)/layout.tsx
+++ frontend/src/app/(dynamic)/layout.tsx
@@ -2,10 +2,9 @@
 import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { hasSession } from '../../services/auth/session'
-import { login, logout, refreshToken } from '../../store/auth/auth'
+import { logout, refreshToken } from '../../store/auth/auth'
 import { useAppDispatch } from '../../store/hooks'
 import { store } from '../../store/store'
-import { SessionData } from '../../types/auth'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
@@ -29,8 +28,7 @@ const Auth: React.FC<AuthProps> = ({ children }) => {
 				return null
 			}
 
-			const sessionPayload = await dispatch(refreshToken({ bypassExpiryCheck: true }))
-			dispatch(login(sessionPayload.payload as SessionData))
+			await dispatch(refreshToken({ bypassExpiryCheck: true }))
 		}
 		checkAuthenticated()
 	}, []) // eslint-disable-line react-hooks/exhaustive-deps
